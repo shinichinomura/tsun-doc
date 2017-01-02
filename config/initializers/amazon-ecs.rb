6878efb1e6ba0a089a3d0197f40b1890bfab4519
Shinichi NOMURA <shinichi.nomura@gmail.com>
2017-01-02 19:01:40 +0900

    amazon-ecs gem を導入

---
+++ config/initializers/amazon-ecs.rb
@@ -0,0 +1,9 @@
+require 'amazon/ecs'
+
+Amazon::Ecs.configure do |options|
+  options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
+  options[:AWS_secret_key] = ENV['AWS_SECRET_ACCESS_KEY']
+  options[:associate_tag] = ENV['AMAZON_ASSOCIATE_TAG']
+end
+
+Amazon::Ecs::debug = true if Rails.env.development?
